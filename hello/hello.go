048c893ce8b193504bbf3fcf531cf3ea2f8bcf96
Tosin Akinnagbe <vandathron>
2020-11-09 10:36:52 +0100

    looping"

--- hello/hello.go
+++ hello/hello.go
@@ -3,8 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/cmplx"
 
 	"example.com/greetings"
+)
+
+var i, j int = 1, 2
+var name string = "tosin"
+var (
+	ToBe   bool       = false
+	maxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
@@ -19,4 +28,35 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(messages)
+	sayhi(calc(10, 5))
+	// whileLoop()
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", maxInt, maxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
+
+func sayhi(x, y, z int) {
+	fmt.Println(addNum(23, 10))
+	fmt.Println(x, y, z)
+}
+
+func addNum(x, y int) int {
+	return x + y
+}
+
+func calc(x, y int) (int, int, int) {
+	add := x + y
+	min := x - y
+	mul := x * y
+	return add, min, mul
+}
+
+//while loop
+
+func whileLoop() {
+	sum := 0
+	for sum < 100 {
+		println(sum)
+		sum += 2
+	}
 }
